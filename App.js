197cf53b1fc384e8a8e2f76ec40317990035c968
Ronit <ronit2001krish@gmail.com>
2024-03-07 20:32:41 +0530

    chapter-01

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement('div', { id: 'parent' }, [
+    React.createElement('div', { id: 'child1' }, [
+        React.createElement('h1', { id: 'heading' }, 'Hello From React!'),
+        React.createElement('h2', {}, 'Hello From React!')
+    ]),
+    React.createElement('div', { id: 'child2' }, [
+        React.createElement('h1', { id: 'heading' }, 'Hello From React!'),
+        React.createElement('h2', {}, 'Hello From React!')
+    ])]
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(root);
+
+root.render(parent);
