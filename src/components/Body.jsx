ccb629050cc0e1a55bcb834311196b02b56d26c1
Ronit <ronit2001krish@gmail.com>
2024-03-15 21:05:40 +0530

    Episode-05

---
+++ src/components/Body.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+export const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="restaurant-container">
+        {listOfRestaurant.map((res) => (
+          <RestaurantCard key={res.info.id} resData={res} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
