8efd8ffe0d2f2a0495d747d9bde69acf03ac3b7b
Ronit <ronit2001krish@gmail.com>
2024-03-30 14:17:03 +0530

    Episode-08

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,18 @@ import React from "react";
 class About extends React.Component {
   constructor(props) {
     super(props);
-    console.log("AC");
   }
 
   componentDidMount(){
-    console.log('ACD')
   }
 
   render() {
-    console.log("AR");
 
     return (
       <div>
         <h1>About</h1>
         <h2>This is a About Page!</h2>
-        <UserClass id={'P'} name={"Krish Thakur (Class)"} />
-        <UserClass id={'Q'} name={"Krish Thakur (Class)"} />
+        <UserClass id={'P'} name={"First"} />
       </div>
     );
   }
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,34 +1,31 @@
 import React from "react";
-import User from "./User";
 
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(`${this.props.id}C`)
     this.state = {
-        count: 0,
+      userInfo: {
+        name: "Dummy",
+        location: "default location",
+      },
     };
-}
-
-componentDidMount(){
-    console.log(`${this.props.id}CD`)
   }
 
-render() {
-      console.log(`${this.props.id}R`)
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/ronitsingh2001");
+    const jsonData = await data.json();
+    this.setState({
+      userInfo: jsonData,
+    });
+  }
+  render() {
+    const { name, location, avatar_url } = this.state.userInfo;
     return (
       <div className="user-card">
-        <h2>Count: {this.state.count}</h2>
-        <button onClick={() => {
-            this.setState({
-                count: this.state.count + 1
-            })
-        }}>Increment</button>
-        <h2>Name: {this.props.name}</h2>
-        <h3>Location: Delhi</h3>
+        <img src={avatar_url} alt="" />
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
         <h4>Contact: @ronit2112</h4>
-        <User id={`${this.props.id}X`} name={"Name001"} />
-        <User id={`${this.props.id}Y`} name={"Name002"} />
       </div>
     );
   }
